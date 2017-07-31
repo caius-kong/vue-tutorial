13b765dbf3c729776bce1748c3a39f371da0835e
孔昀晖 <1032316751@qq.com>
2017-07-31 17:43:56 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import VueResource from 'vue-resource'
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.use(VueResource)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app', // 入口div，index.html
+  router, // 组件路由
+  store,
+  ...App
+  // template: '<App/>', // 独立构建<App></App>
+  // components: { App } // 上面这个template所指的组件
+})
